d9452da71a66faa7fb73eeab86726435b0deb6ad
Amandasulistiyowati <sulistiyowatiamanda@gmail.com>
2023-11-10 09:54:31 +0700

    Menambahkan task 2

---
+++ task2.ts
@@ -0,0 +1,19 @@
+// Problem 3
+
+let nama: string = "Kobar"
+console.log(`Hello ${nama}! Saya Javascript bahasa yang sangat menyenangkan`)
+
+//Problem 4
+
+let alas: number = 20
+let tinggi: number = 25
+console.log(0.5 * alas * tinggi)
+
+//Problem 5
+
+let T: number = 20
+let r: number = 4
+let luaslingkaranalas = 3.14 * Math.pow(r, 2)
+let kelilinglingkaranalas = 2 * 3.14 * r
+let luaspermukaansisitabung = kelilinglingkaranalas * T
+console.log(2 * luaslingkaranalas + luaspermukaansisitabung)
